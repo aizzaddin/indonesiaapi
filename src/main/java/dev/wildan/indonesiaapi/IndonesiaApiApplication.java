edfdb3b4c6197113d82d2cc7fd6d7d472cbde6dd
Wildan Aizzaddin <39244724+aizzaddin@users.noreply.github.com>
2023-05-28 08:41:11 +0700

    Initial Project

---
+++ src/main/java/dev/wildan/indonesiaapi/IndonesiaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.wildan.indonesiaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndonesiaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndonesiaApiApplication.class, args);
+    }
+
+}
